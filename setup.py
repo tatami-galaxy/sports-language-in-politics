08c12c548f3a7bf4cf550f847a44ad168bd4fb9d
ujan <ujandev@gmail.com>
2023-11-13 14:54:41 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Detecting and analyzing the use of sports language in online political discussions',
+    author='Ujan',
+    license='MIT',
+)
